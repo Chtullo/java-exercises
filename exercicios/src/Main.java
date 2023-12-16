3e679d2fc7e108e213fca1fa47aee204bf363095
Nicholas Castro <contabrpt2@hotmail.com>
2023-12-16 12:03:29 -0300

    Add files via upload

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numeroAleatorio = new Random().nextInt(101);
-//                System.out.println(numeroAleatorio);
-        int chute = scanner.nextInt();
-        int numeroDeTentativas = 0;
-
-
         System.out.println("Tente advinhar um número entre 0 e 100");
 
 
+        int numeroAleatorio = new Random().nextInt(101);
+        int numeroDeTentativas = 5;
+        for (int tentativa = 1; tentativa <= numeroDeTentativas; tentativa ++){
+            System.out.println("Tentativa " + tentativa + ":");
+            int chute = scanner.nextInt();
 
-        if (chute == numeroAleatorio){
+        if (chute == numeroAleatorio) {
             System.out.println("Parabéns, você acertou!");
+            break;
+        } else if (chute < numeroAleatorio) {
+            System.out.println("O número secreto é maior que " + chute);
+        } else {
+            System.out.println("O número secreto é menor que " + chute);
         }
 
-
-
+        if (tentativa == numeroDeTentativas) {
+            System.out.println("Suas tentativas acabaram! O número secreto era: " + numeroAleatorio);
+        }
     }
+   }
 }
